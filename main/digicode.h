f4c4bfcfb7a82b2127a6410c6c9a44dce90bddcf
LamaNivelle <hectordetok@gmail.com>
2021-12-03 11:59:53 +0100

    update

--- main/digicode.h
+++ main/digicode.h
@@ -1,8 +1,5 @@
 #ifndef DIGICODE_H
 #define DIGICODE_H
-#include <LiquidCrystal_I2C.h>
-
-extern LiquidCrystal_I2C lcd;
 
 void initialization();
 void checkPassword();
--- main/main.ino
+++ main/main.ino
@@ -2,8 +2,7 @@
 #include <Wire.h>
 
 #include "rgb_lcd.h"
-#include "piches_h"
-#include "digicode_h"
+#include "pitches.h"
 
 //initialize and define GLOBAL VARIABLE
 String password = "hello";
@@ -14,11 +13,38 @@ int colorR;
 int colorG;
 int colorB;
 
+//define my functions
+
+void initialization(){
+  lcd.clear();
+  lcd.setRGB(255,255,255);
+  checkPassword();
+}
+
+void checkPassword(){
+  String inputPassword;
+  int errorCounter=0;
+  
+  lcd.clear(); lcd.setCursor(1,0); lcd.print("Enter password:");
+  if(Serial.available()){
+    inputPassword = Serial.readStringUntil('\n');
+    if(inputPassword == password){
+      lcd.clear(); lcd.setCursor(4,1); lcd.setRGB(0,255,0); lcd.print("Correct");
+    } else if (inputPassword == "#"){
+      changePassword();
+    } else errorCounter++;
+  }
+}
+
+void changePassword(){
+  lcd.clear(); lcd.setCursor(0,0); lcd.print("Enter new password:");
+}
+
+
 void setup() {
   Serial.begin(9600);   //initialize the serial monitor
-
-  lcd.init();    //initialize the lcd screen
-  lcd.backlight();
+  lcd.begin(16,2);
+  lcd.setRGB(colorR, colorG, colorB);
 
 }
 
